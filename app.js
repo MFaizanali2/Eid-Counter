a6f77846b6ec7c1358d2aad4b599f4feeebb5aee
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-28 22:30:35 +0500

    completed
    continue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let eid_date = new Date("june 16, 2024 00:00:00").getTime();
 
 let countdown = setInterval(() => {
 
-    let number = document.getElementById(number);
+    // let number = document.getElementById(number);
 
     // define current date
     let now = new Date().getTime();
@@ -12,10 +12,16 @@ let countdown = setInterval(() => {
     let diff = eid_date - now ;
 
     // get day hour time and second
-    let day = Math.floor(diff / (1000 * 60 *60 *24));
-    let hour = Math.floor(diff % (1000 * 60 *60 *24) / (1000 *60 *60));
-    let minute = Math.floor(diff % (1000 * 60 *60 ) / (1000 *60));
-    let second = Math.floor(diff % (1000 * 60 ) / (1000));
-    number.value = `${day} ${hour} ${minute} ${second}`
+    let day = Math.floor(diff / (1000 * 60 *60 *24)).toString().padStart(2,0);
+    let hour = Math.floor((diff % (1000 * 60 *60 *24)) / (1000 *60 *60)).toString().padStart(2,0);
+    let minute = Math.floor((diff % (1000 * 60 *60 )) / (1000 *60)).toString().padStart(2,0);
+    let second = Math.floor((diff % (1000 * 60 )) / (1000)).toString().padStart(2,0);
+    let count = `${day} : ${hour} : ${minute} : ${second}`;
+    document.getElementById("number").textContent = count;
+
+    if (diff < 0) {
+        clearInterval(countdown);
+        document.getElementById("number").innerHTML = "Eid Mubarak!";
+    }
 
 },1000)
