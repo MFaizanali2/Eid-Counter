0cb4a7ac65cb3fe84c3dcdb27b4fb5362a1a5a79
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-28 21:39:17 +0500

    still work
    continue

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-let number = document.getElementById("number").value;
-let date = new Date();
+
 let eid_date = new Date("june 16, 2024 00:00:00").getTime(); 
+
+let countdown = setInterval(() => {
+
+    let number = document.getElementById(number);
+
+    // define current date
+    let now = new Date().getTime();
+
+    // difference current and future date
+    let diff = eid_date - now ;
+
+    // get day hour time and second
+    let day = Math.floor(diff / (1000 * 60 *60 *24));
+    let hour = Math.floor(diff % (1000 * 60 *60 *24) / (1000 *60 *60));
+    let minute = Math.floor(diff % (1000 * 60 *60 ) / (1000 *60));
+    let second = Math.floor(diff % (1000 * 60 ) / (1000));
+    number.value = `${day} ${hour} ${minute} ${second}`
+
+},1000)
